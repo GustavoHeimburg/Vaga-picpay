0d1e23ca8a4132787cf8d00ed0243e0eb7b64b8e
GustavoHeimburg <gutogustavogu@gmail.com>
2024-01-23 14:05:47 -0300

    Commit

---
+++ src/main/java/com/example/vagapicpay/VagaPicpayApplication.java
@@ -0,0 +1,13 @@
+package com.example.vagapicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagaPicpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VagaPicpayApplication.class, args);
+    }
+
+}
