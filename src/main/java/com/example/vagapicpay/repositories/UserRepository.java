2ed4e0096f92754138de988f488caa579b1b86d5
GustavoHeimburg <gutogustavogu@gmail.com>
2024-02-03 13:27:45 -0300

    Init back-end

---
+++ src/main/java/com/example/vagapicpay/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.vagapicpay.repositories;
+
+import com.example.vagapicpay.user.User;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+
+public interface UserRepository  {
+}
--- src/main/java/com/example/vagapicpay/user/User.java
+++ src/main/java/com/example/vagapicpay/user/User.java
@@ -1,4 +1,6 @@
 package com.example.vagapicpay.user;
 
 public class User {
+
+    private long id;
 }
