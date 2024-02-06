64a9ed626a3c3a571a723dbb8a8fe4922c6d1c6a
GustavoHeimburg <gutogustavogu@gmail.com>
2024-02-05 22:37:48 -0300

    Init back-end

--- src/main/java/com/example/vagapicpay/controller/User.java
+++ src/main/java/com/example/vagapicpay/controller/User.java
@@ -3,4 +3,22 @@ package com.example.vagapicpay.controller;
 public class User {
 
     private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
 }
