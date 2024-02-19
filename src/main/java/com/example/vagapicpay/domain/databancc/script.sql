7647b4c38360f681ee1d6da50325806e2917b046
GustavoHeimburg <gutogustavogu@gmail.com>
2024-02-18 22:17:04 -0300

    Move package

--- src/main/java/com/example/vagapicpay/domain/databancc/script.sql
+++ src/main/java/com/example/vagapicpay/domain/databancc/script.sql
@@ -3,6 +3,9 @@ use person;
 
 create table client(
 
-    name varchar
+    id bigint (15) AUTO_INCREMENT not null,
+    name varchar (45) unique not null,
+    idade int (10) not null,
+    senha int (60) unique not null
 
 );
